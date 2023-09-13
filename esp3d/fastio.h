671c206e030de2cc68ac9fc3d0ef6f640c937abd
luhill <freeflyhill@gmail.com>
2023-09-13 14:10:04 +1000

    added i2s expansion code

---
+++ esp3d/fastio.h
@@ -0,0 +1,67 @@
+
+#pragma once
+
+#include "i2s.h"
+#include "macros.h"
+/**
+ * Utility functions
+ */
+
+// I2S expander pin mapping.
+#define IS_I2S_EXPANDER_PIN(IO) TEST(IO, 7)
+#define I2S_EXPANDER_PIN_INDEX(IO) (IO & 0x7F)
+
+// Set pin as input
+#define _SET_INPUT(IO)          pinMode(IO, INPUT)
+
+// Set pin as output
+#define _SET_OUTPUT(IO)         pinMode(IO, OUTPUT)
+
+// Set pin as input with pullup mode
+#define _PULLUP(IO, v)          pinMode(IO, v ? INPUT_PULLUP : INPUT)
+
+// Read a pin wrapper
+#define READ(IO)                (IS_I2S_EXPANDER_PIN(IO) ? i2s_state(I2S_EXPANDER_PIN_INDEX(IO)) : digitalRead(IO))
+
+// Write to a pin wrapper
+#define WRITE(IO, v)            (IS_I2S_EXPANDER_PIN(IO) ? i2s_write(I2S_EXPANDER_PIN_INDEX(IO), v) : digitalWrite(IO, v))
+
+// Set pin as input wrapper
+#define SET_INPUT(IO)           _SET_INPUT(IO)
+
+// Set pin as input with pullup wrapper
+#define SET_INPUT_PULLUP(IO)    do{ _SET_INPUT(IO); _PULLUP(IO, HIGH); }while(0)
+
+// Set pin as input with pulldown (substitution)
+#define SET_INPUT_PULLDOWN      SET_INPUT
+
+// Set pin as output wrapper
+#define SET_OUTPUT(IO)          do{ _SET_OUTPUT(IO); }while(0)
+
+// Set pin as PWM
+#define SET_PWM                 SET_OUTPUT
+
+// Set pin as output and init
+#define OUT_WRITE(IO,V)         do{ _SET_OUTPUT(IO); WRITE(IO,V); }while(0)
+
+// digitalRead/Write wrappers
+#define extDigitalRead(IO)      digitalRead(IO)
+#define extDigitalWrite(IO,V)   digitalWrite(IO,V)
+
+// PWM outputs
+#define PWM_PIN(P)              (P < 34 || P > 127) // NOTE Pins >= 34 are input only on ESP32, so they can't be used for output.
+
+// Toggle pin value
+#define TOGGLE(IO)              WRITE(IO, !READ(IO))
+
+//
+// Ports and functions
+//
+
+// UART
+#define RXD        3
+#define TXD        1
+
+// TWI (I2C)
+#define SCL        5
+#define SDA        4
--- esp3d/pool.cpp
+++ esp3d/pool.cpp
@@ -4,121 +4,12 @@
 #include <time.h>
 #include <sys/time.h>
 #include "espcom.h"
+#include "pins_tinybee.h"
+#include "i2s.h"
+
+#include "fastio.h"
 /*-----MKS-TinyBee Pins---------------------------------------------------------------------------------*/
-#define SERVO0_PIN                            2
 
-//
-// Limit Switches
-//
-#define X_STOP_PIN                            33
-#define Y_STOP_PIN                            32
-#define Z_STOP_PIN                            22
-//#define FIL_RUNOUT_PIN                        35
-
-//
-// Enable I2S stepper stream
-//
-#undef I2S_STEPPER_STREAM
-#define I2S_STEPPER_STREAM
-#define I2S_WS                                26
-#define I2S_BCK                               25
-#define I2S_DATA                              27
-#undef LIN_ADVANCE                                // Currently, I2S stream does not work with linear advance
-
-//
-// Steppers
-//
-#define X_STEP_PIN                           129
-#define X_DIR_PIN                            130
-#define X_ENABLE_PIN                         128
-
-#define Y_STEP_PIN                           132
-#define Y_DIR_PIN                            133
-#define Y_ENABLE_PIN                         131
-
-#define Z_STEP_PIN                           135
-#define Z_DIR_PIN                            136
-#define Z_ENABLE_PIN                         134
-
-#define E0_STEP_PIN                          138
-#define E0_DIR_PIN                           139
-#define E0_ENABLE_PIN                        137
-
-#define E1_STEP_PIN                          141
-#define E1_DIR_PIN                           142
-#define E1_ENABLE_PIN                        140
-
-#define Z2_STEP_PIN                          141
-#define Z2_DIR_PIN                           142
-#define Z2_ENABLE_PIN                        140
-
-//
-// Temperature Sensors
-//
-#define TEMP_0_PIN                            36  // Analog Input
-#define TEMP_1_PIN                            34  // Analog Input, you need set R6=0Ω and R7=NC
-#define TEMP_BED_PIN                          39  // Analog Input
-
-//
-// Heaters / Fans
-//
-#define HEATER_0_PIN                         145
-#ifndef MKS_TEST
-#define HEATER_1_PIN                         146
-#define FAN_PIN                              147
-#define FAN1_PIN                             148
-#endif
-#define HEATER_BED_PIN                       144
-
-//#define CONTROLLER_FAN_PIN                 148
-//#define E0_AUTO_FAN_PIN                    148  // need to update Configuration_adv.h @section extruder
-//#define E1_AUTO_FAN_PIN                    149  // need to update Configuration_adv.h @section extruder
-#ifdef MKS_TEST
-#define HEATER_1_PIN_T                         146
-#define FAN_PIN_T                              147
-#define FAN1_PIN_T                             148
-#define HEATER_BED_PIN_T                       144
-#endif
-
-
-//
-// MicroSD card
-//
-#define SD_MOSI_PIN                           23
-#define SD_MISO_PIN                           19
-#define SD_SCK_PIN                            18
-#define SDSS                                   5
-#define SD_DETECT_PIN                         34 // IO34 default is SD_DET signal(Jump to SDDET)
-#define USES_SHARED_SPI                       // SPI is shared by SD card with TMC SPI drivers
-
-/**
- *                _____                                             _____
- * (BEEPER)IO149 | · · | IO13(BTN_ENC)             (SPI MISO) IO19 | · · | IO18 (SPI SCK)
- *  (LCD_EN)IO21 | · · | IO4(LCD_RS)                (BTN_EN1) IO14 | · · | IO5 (SPI CS)
- *  (LCD_D4)IO0  | · ·   IO16(LCD_D5)               (BTN_EN2) IO12 | · ·   23 (SPI MOSI)
- *  (LCD_D6)IO15 | · · | IO17(LCD_D7)               (SPI_DET) IO34 | · · | RESET
- *           GND | · · | 5V                                    GND | · · | 3.3V
- *                ￣￣￣                                            ￣￣￣
- *                EXP1                                               EXP2
- */
-
-#define EXP1_03_PIN                         17
-#define EXP1_04_PIN                         15
-#define EXP1_05_PIN                         16
-#define EXP1_06_PIN                         0
-#define EXP1_07_PIN                         4
-#define EXP1_08_PIN                         21
-#define EXP1_09_PIN                         13
-#define EXP1_10_PIN                         149
-
-#define EXP2_03_PIN                         -1    // RESET
-#define EXP2_04_PIN                         34
-#define EXP2_05_PIN                         23
-#define EXP2_06_PIN                         12
-#define EXP2_07_PIN                         5
-#define EXP2_08_PIN                         14
-#define EXP2_09_PIN                         18
-#define EXP2_10_PIN                         19
 /*------------------------------------------------------------------------------------------------------*/
 
 #define PIN_PUMP_POWER          26
@@ -166,12 +57,15 @@ static int nextAlarmSeconds;
 POOL::POOL(){}
 
 void POOL::setup(uint32_t cycle_period_ms){
-    pinMode(PIN_PUMP_POWER,OUTPUT);
+    int i = i2s_init();
+
+    //WRITE(Y_STEP_PIN,HIGH);
+    /*pinMode(PIN_PUMP_POWER,OUTPUT);
     pinMode(PIN_CHLORINE_DUTY_FWD,OUTPUT);
     pinMode(PIN_CHLORINE_DUTY_REV,OUTPUT);
     pinMode(PIN_ION_DUTY_FWD,OUTPUT);
     pinMode(PIN_ION_DUTY_REV,OUTPUT);
-
+  */
     setupClock();
     EEPROM.get(POOL_PARAMETERS_ADDRESS,poolSettings);
     if(!poolSettings.initialized){
@@ -219,6 +113,11 @@ void POOL::setup(uint32_t cycle_period_ms){
     ledcAttachPin(PIN_ION_DUTY_REV, ionPWMChannel_rev);
 
     setOutputs();
+    SET_OUTPUT(X_ENABLE_PIN);
+    WRITE(X_ENABLE_PIN,128);
+    //i2s_push_sample();
+    //pinMode(Y_STEP_PIN,OUTPUT);
+    //i2s_write(0, HIGH);
 }
 void POOL::loop(){
 }
@@ -316,9 +215,9 @@ bool POOL::getIonStatus(){
   return ionOn;
 }
 void POOL::setOutputs(){
-  digitalWrite(PIN_PUMP_POWER, pumpOn);
-  setChlorine();
-  setIon();
+  //digitalWrite(PIN_PUMP_POWER, pumpOn);
+  //setChlorine();
+  //setIon();
 }
 void POOL::setChlorine(){
   ledcWrite(chlorinePWMChannel_fwd,poolSettings.chlorine_duty*255/100*chlorineOn*!chlorine_rev);
